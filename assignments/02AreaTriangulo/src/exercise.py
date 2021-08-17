e95b04c561a6c4f6fb713416701893625b32dbcd
A01740723 <A01740723@itesm.mx>
2021-08-17 02:15:54 +0000

    Tarea 1

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    b = float(input('Ingresa medida de la base:'))
+    h = float(input('Ingresa medida de la altura'))
 
+    area = (b*h)/2
+    print('el area es: '+ str(area))
 if __name__ == '__main__':
     main()
--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,7 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    Mat1 = float(input("ingresa calificacion de materia: "))
+    Mat2 = float(input("ingresa calificacion de materia: "))
+    Mat3 = float(input("ingresa calificacion de materia: "))
+    Mat4 = float(input("ingresa calificacion de materia: "))
+    promedio = (Mat1 + Mat2 + Mat3 + Mat4)/4
 
+    print("el promedio general es: " + str(promedio))
 
 if __name__ == '__main__':
     main()
