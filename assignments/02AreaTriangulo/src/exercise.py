d15ac15f200c1765062352c0bf0925bc7583c838
A01740723 <A01740723@itesm.mx>
2021-08-18 02:35:18 +0000

    Tarea 1 resuelta

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     #escribe tu código abajo de esta línea
     b = float(input('Ingresa medida de la base:'))
-    h = float(input('Ingresa medida de la altura'))
+    h = float(input('Ingresa medida de la altura: '))
 
     area = (b*h)/2
     print('el area es: '+ str(area))
